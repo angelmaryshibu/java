3021d5f7c561504fbc6e03e948c4b030c1e1712e
angelmaryshibu <86075805+angelmaryshibu@users.noreply.github.com>
2022-03-01 14:41:21 +0530

    Update abstract1.java

--- abstract1.java
+++ abstract1.java
@@ -1,1 +1,39 @@
+import java.util.*;
+abstract class shapes
+{
+    abstract void sides();
+}
+class rectangel extends shapes
+{
+    void sides()
+    {
+        System.out.println("rectangel has 3 sides");
+    }
+}
+class triangle extends shapes
+{
+    void sides()
+    {
+        System.out.println("triangle has 3 sides");
+    }
+}
+class hexagon extends shapes
+{
+    void sides()
+    {
+        System.out.print("hexagon has 3 sides");
+    }
+}
+public class abstract1
+{
+	public static void main(String[] args) 
+	{
+		rectangel r=new rectangel();
+		triangle t=new triangle();
+		hexagon h=new hexagon();
+		r.sides();
+		t.sides();
+		h.sides();
+	}
+}
 
